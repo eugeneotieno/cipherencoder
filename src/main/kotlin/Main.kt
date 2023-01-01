87d97cae6961b75d89320145d9c998bb205f2201
Eugene Otieno <otienoeo@gmail.com>
2023-01-01 17:44:01 +0300

    Convert character to binary

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,17 @@
 fun main() {
+    println("Input string:")
     val inputString = readln()
+
+    println()
+    println("The result:")
+
     for (c in inputString) {
-        print("$c ")
+        println("$c = ${charTo7BitBinary(c)}")
     }
+}
+
+fun charTo7BitBinary(c: Char): String {
+    val intValue = c.code
+    val binaryString = Integer.toBinaryString(intValue)
+    return binaryString.padStart(7, '0')
 }
